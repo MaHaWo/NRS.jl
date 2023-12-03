a6531a675987c34164e54afcac5ddc5fb83791db
Harald Mack <harald.mack@protonmail.com>
2023-12-03 21:06:28 +0100

    add test skeletons for rewriting dense

--- test/dense_rule_test.jl
+++ test/dense_rule_test.jl
@@ -1,9 +1,11 @@
 
+
 include("../src/NRS.jl")
 
 include("./testhelpers.jl")
 
 using Test
+
 using SparseArrayKit
 
 @testset "dense_rule_construction_test" begin
@@ -30,5 +32,49 @@ using SparseArrayKit
 end
 
 
-@testset "dense_rule_rewriting_test" begin 
+@testset "dense_rule_function_test" begin 
+    data = make_ruletest_data(Array)
+
+    net = NRS.BasicDenseNet(10, 10, 4, data.code)
+    rule = NRS.DenseRule(1, 15, 15, 4, NRS.redistribute_marking_conserved, data.code)
+
+    NRS.rebuild_net!(rule, net)
+
+    @test net.input ≈ data.net_input
+
+    @test net.output≈ data.net_output 
+
+    @test net.marking≈ data.net_marking
+
+    @test NRS.count_nonzero_target_elements(rule) == Dict("in" => 0, "out" => 1)
+
+    @test NRS.count_nonzero_effect_elements(rule) == Dict("in" => 4, "out" => 2)
+    
+    @test NRS.compute_enabled_rule!(rule; nets= [net,], with_control_marking=true, with_weight_check=true)
+
+end
+
+
+@testset "rewrite_conserved" begin 
+    data = make_ruletest_data(Array)
+
+    net = NRS.BasicDenseNet(10, 10, 4, data.code)
+    rule = NRS.DenseRule(1, 15, 15, 4, NRS.redistribute_marking_conserved!, data.code)
+
+    NRS.rewrite!(rule, net, nets = [net,], with_control_marking = true, with_weight_check = true)
+
+    # TODO: add actual tests
+end 
+
+
+@testset "rewrite_copy" begin 
+    data = make_ruletest_data(Array)
+
+    net = NRS.BasicDenseNet(10, 10, 4, data.code)
+    rule = NRS.DenseRule(1, 15, 15, 4, NRS.redistribute_marking_copy!, data.code)
+
+    NRS.rewrite!(rule, net, nets = [net,], with_control_marking = true, with_weight_check = true)
+
+    # TODO: add actual tests
+
 end
