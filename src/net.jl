14ee65ee940b01ad7474b74362dc4fc6dee0a1b6
Harald Mack <harald.mack@protonmail.com>
2024-10-08 19:36:16 +0200

    add net skeleton

--- src/net.jl
+++ src/net.jl
@@ -1,17 +1,38 @@
 
+include("./encoding.jl")
 
 mutable struct Net{V, T <: AbstractArray}
     input::T{V}
     output::T{V}
     enabled::T{bool}
-    state:T{V}
-    _state:T{V}
+    state::T{V}
+    _state::T{V}
 end
 
-function resolve_conflict!(net: Net) end
+function Net() end
+function Net() end
+function Net() end
 
-function compute_enabled!(net::Net) end
+function _check_enabled(net::Net)::Bool
+    return all(net.enabled .== true)
+end
 
-function step!(net::Net) end
+function _compute_step!(net::Net)::Nothing end
 
-function run!(net::Net) end
+function resolve_conflict!(net: Net)::Nothing end
+
+function compute_enabled!(net::Net)::Nothing end
+
+function step!(net::Net)::Nothing
+    compute_enabled!(net)
+    resolve_conflict!(net)
+    _compute_step!(net)
+end
+
+function run!(net::Net, num_steps::Int32)::Nothing
+    t::Int32 = 0;
+    while _check_enabled(net) && t < num_step
+        step!(net)
+        t+=1
+    end
+end
