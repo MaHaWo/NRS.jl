5d651c38515c51a3c3068e183a026dc595ce3f25
Harald Mack <harald.mack@protonmail.com>
2025-08-28 20:28:16 +0200

    restart everything

---
+++ src/net.jl
@@ -0,0 +1,8 @@
+function compute_conflicts(net::net) end 
+
+function resolve_conflicts!(net::Net, nrs::NRS, conflicts::Vector{Tuple{Int,Int}}) end
+
+function compute_enabled!(net::Net) end
+
+
+function step!(net::Net) end
--- src/types.jl
+++ src/types.jl
@@ -1,54 +1,28 @@
-########################################################################################################################
-## Abstract utility types
-abstract type AbstractSymmetry end
+struct AbstractNet end 
+struct AbstractRule end
+struct AbstractEdgeList end 
+struct AbstractNRS end
 
-########################################################################################################################
-## Abstract token types
-abstract type AbstractToken end
+struct EdgeList{T <: AbstractArray} <: AbstractEdgeList
+    nodes::Vector{Int}
+    weights::T
+end
 
-abstract type AbstractRuleToken <: AbstractToken end
+struct Net{T <: AbstractArray, P <: AbstractArray} <: AbstractNet
+    inputs::T
+    outputs::T
+    markings::P 
+    enabled::Vector{Bool}
+end
 
-abstract type AbstractEnergyToken <: AbstractToken end
+struct Rule{T <: AbstractArray} <: AbstractRule
+    conditions::EdgeList{T}
+    actions::EdgeList{T}
+end
 
-abstract type AbstractEnergyRuleToken <: AbstractRuleToken end 
-
-########################################################################################################################
-## Abstract net types
-
-abstract type AbstractNet end
-
-abstract type AbstractDiscreteNet <: AbstractNet end 
-
-abstract type AbstractContinuousNet <: AbstractNet end 
-
-abstract type  AbstractDenseDiscreteNet <: AbstractDiscreteNet end 
-
-abstract type  AbstractSparseDiscreteNet <: AbstractDiscreteNet  end 
-
-
-abstract type  AbstractBasicDenseDiscreteNet <: AbstractDenseDiscreteNet  end 
-
-abstract type  AbstractBasicSparseDiscreteNet <: AbstractSparseDiscreteNet  end 
+struct NRS{T <: AbstractArray} <: AbstractNRS
+    net::Net{T}
+    rules::Rule{T}
+end
 
 
-abstract type  AbstractEnergyDenseDiscreteNet <: AbstractDenseDiscreteNet  end 
-
-abstract type  AbstractEnergySparseDiscreteNet <: AbstractSparseDiscreteNet  end 
-
-
-########################################################################################################################
-## Abstract rewriting rule types
-abstract type AbstractRule end
-
-abstract type AbstractSparseRule <: AbstractRule end 
-
-abstract type AbstractDenseRule <: AbstractRule end 
-
-
-########################################################################################################################
-## Abstract rewriting system types
-abstract type AbstractNetRewritingSystem end 
-
-abstract type AbstractDenseNetRewritingSystem <: AbstractNetRewritingSystem end 
-
-abstract type AbstractSparseNetRewritingSystem <: AbstractNetRewritingSystem end 
